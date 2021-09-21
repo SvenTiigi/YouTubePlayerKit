bd96bea3a841570380dec3dcbcd5cc6a22490ce0
Sven Tiigi <sven.tiigi@gmail.com>
2021-09-21 09:17:02 +0200

    Moved YouTubePlayerAPI implementation of YouTubePlayer to a dedicated file

---
+++ Sources/YouTubePlayer+API.swift
@@ -0,0 +1,340 @@
+import Combine
+import Foundation
+
+// MARK: - YouTubePlayerConfigurationAPI
+
+extension YouTubePlayer: YouTubePlayerConfigurationAPI {
+    
+    /// Update YouTubePlayer Configuration
+    /// - Note: Updating the Configuration will result in a reload of the entire YouTubePlayer
+    /// - Parameter configuration: The YouTubePlayer Configuration
+    public func update(
+        configuration: YouTubePlayer.Configuration
+    ) {
+        self.api?.update(configuration: configuration)
+    }
+    
+}
+
+// MARK: - YouTubePlayerLoadAPI
+
+extension YouTubePlayer: YouTubePlayerLoadAPI {
+    
+    /// Load YouTubePlayer Source
+    /// - Parameter source: The YouTubePlayer Source to load
+    public func load(
+        source: YouTubePlayer.Source
+    ) {
+        self.api?.load(source: source)
+    }
+    
+}
+
+// MARK: - YouTubePlayerEventAPI
+
+extension YouTubePlayer: YouTubePlayerEventAPI {
+    
+    /// The current YouTubePlayer State, if available
+    public var state: YouTubePlayer.State? {
+        self.api?.state
+    }
+    
+    /// A Publisher that emits the current YouTubePlayer State
+    public var statePublisher: AnyPublisher<State, Never> {
+        self.objectWillChange
+            .flatMap { [weak self] in
+                self?.api?.statePublisher
+                    ?? Empty().eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    /// The current YouTubePlayer VideoState, if available
+    public var videoState: YouTubePlayer.VideoState? {
+        self.api?.videoState
+    }
+
+    /// A Publisher that emits the current YouTubePlayer VideoState
+    public var videoStatePublisher: AnyPublisher<VideoState, Never> {
+        self.objectWillChange
+            .flatMap { [weak self] in
+                self?.api?.videoStatePublisher
+                    ?? Empty().eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    /// The current YouTubePlayer PlaybackQuality, if available
+    public var playbackQuality: YouTubePlayer.PlaybackQuality? {
+        self.api?.playbackQuality
+    }
+    
+    /// A Publisher that emits the current YouTubePlayer PlaybackQuality
+    public var playbackQualityPublisher: AnyPublisher<PlaybackQuality, Never> {
+        self.objectWillChange
+            .flatMap { [weak self] in
+                self?.api?.playbackQualityPublisher
+                    ?? Empty().eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    /// The current YouTubePlayer PlaybackRate, if available
+    public var playbackRate: YouTubePlayer.PlaybackRate? {
+        self.api?.playbackRate
+    }
+    
+    /// A Publisher that emits the current YouTubePlayer PlaybackRate
+    public var playbackRatePublisher: AnyPublisher<PlaybackRate, Never> {
+        self.objectWillChange
+            .flatMap { [weak self] in
+                self?.api?.playbackRatePublisher
+                    ?? Empty().eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+}
+
+// MARK: - YouTubePlayerVideoAPI
+
+extension YouTubePlayer: YouTubePlayerVideoAPI {
+    
+    /// Plays the currently cued/loaded video
+    public func play() {
+        self.api?.play()
+    }
+    
+    /// Pauses the currently playing video
+    public func pause() {
+        self.api?.pause()
+    }
+    
+    /// Stops and cancels loading of the current video
+    public func stop() {
+        self.api?.stop()
+    }
+    
+    /// Seeks to a specified time in the video
+    /// - Parameters:
+    ///   - seconds: The seconds parameter identifies the time to which the player should advance
+    ///   - allowSeekAhead: Determines whether the player will make a new request to the server
+    public func seek(
+        to seconds: Double,
+        allowSeekAhead: Bool
+    ) {
+        self.api?.stop()
+    }
+    
+}
+
+// MARK: - YouTubePlayer360DegreePerspectiveAPI
+
+extension YouTubePlayer: YouTubePlayer360DegreePerspectiveAPI {
+    
+    /// Retrieves properties that describe the viewer's current perspective
+    /// - Parameter completion: The completion closure
+    public func get360DegreePerspective(
+        completion: @escaping (Result<Perspective360Degree, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.get360DegreePerspective(completion: completion)
+    }
+    
+    /// Sets the video orientation for playback of a 360° video
+    /// - Parameter perspective360Degree: The Perspective360Degree
+    public func set(
+        perspective360Degree: Perspective360Degree
+    ) {
+        self.api?.set(perspective360Degree: perspective360Degree)
+    }
+    
+}
+
+// MARK: - YouTubePlayerPlaylistAPI
+
+extension YouTubePlayer: YouTubePlayerPlaylistAPI {
+    
+    /// This function loads and plays the next video in the playlist
+    public func nextVideo() {
+        self.api?.nextVideo()
+    }
+    
+    /// This function loads and plays the previous video in the playlist
+    public func previousVideo() {
+        self.api?.previousVideo()
+    }
+    
+    /// This function loads and plays the specified video in the playlist
+    /// - Parameter index: The index of the video that you want to play in the playlist
+    public func playVideo(
+        at index: UInt
+    ) {
+        self.api?.playVideo(at: index)
+    }
+    
+    /// This function indicates whether the video player should continuously play a playlist
+    /// or if it should stop playing after the last video in the playlist ends
+    /// - Parameter enabled: Bool value if is enabled
+    public func setLoop(
+        enabled: Bool
+    ) {
+        self.api?.setLoop(enabled: enabled)
+    }
+    
+    /// This function indicates whether a playlist's videos should be shuffled
+    /// so that they play back in an order different from the one that the playlist creator designated
+    /// - Parameter enabled: Bool value if is enabled
+    public func setShuffle(
+        enabled: Bool
+    ) {
+        self.api?.setShuffle(enabled: enabled)
+    }
+    
+    /// This function returns an array of the video IDs in the playlist as they are currently ordered
+    /// - Parameter completion: The completion closure
+    public func getPlaylist(
+        completion: @escaping (Result<[String], YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getPlaylist(completion: completion)
+    }
+    
+    /// This function returns the index of the playlist video that is currently playing.
+    /// - Parameter completion: The completion closure
+    public func getPlayistIndex(
+        completion: @escaping (Result<UInt, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getPlayistIndex(completion: completion)
+    }
+    
+}
+
+// MARK: - YouTubePlayerVolumeAPI
+
+extension YouTubePlayer: YouTubePlayerVolumeAPI {
+    
+    /// Mutes the player
+    public func mute() {
+        self.api?.mute()
+    }
+    
+    /// Unmutes the player
+    public func unmute() {
+        self.api?.unmute()
+    }
+    
+    /// Returns Bool value if the player is muted
+    /// - Parameter completion: The completion closure
+    public func isMuted(
+        completion: @escaping (Result<Bool, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.isMuted(completion: completion)
+    }
+    
+    /// Returns the player's current volume, an integer between 0 and 100
+    /// - Parameter completion: The completion closure
+    public func getVolume(
+        completion: @escaping (Result<UInt, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getVolume(completion: completion)
+    }
+    
+    /// Sets the volume.
+    /// Accepts an integer between 0 and 100
+    /// - Parameter volume: The volume
+    public func set(
+        volume: UInt
+    ) {
+        self.api?.set(volume: volume)
+    }
+    
+}
+
+// MARK: - YouTubePlayerPlaybackRateAPI
+
+extension YouTubePlayer: YouTubePlayerPlaybackRateAPI {
+    
+    /// This function retrieves the playback rate of the currently playing video
+    /// - Parameter completion: The completion closure
+    public func getPlaybackRate(
+        completion: @escaping (Result<PlaybackRate, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getPlaybackRate(completion: completion)
+    }
+    
+    /// This function sets the suggested playback rate for the current video
+    /// - Parameter playbackRate: The playback rate
+    public func set(
+        playbackRate: PlaybackRate
+    ) {
+        self.api?.set(playbackRate: playbackRate)
+    }
+    
+    /// This function returns the set of playback rates in which the current video is available
+    /// - Parameter completion: The completion closure
+    public func getAvailablePlaybackRates(
+        completion: @escaping (Result<[PlaybackRate], YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getAvailablePlaybackRates(completion: completion)
+    }
+    
+}
+
+// MARK: - YouTubePlayerPlaybackAPI
+
+extension YouTubePlayer: YouTubePlayerPlaybackAPI {
+    
+    /// Returns a number between 0 and 1 that specifies the percentage of the video that the player shows as buffered
+    /// - Parameter completion: The completion closure
+    public func getVideoLoadedFraction(
+        completion: @escaping (Result<Double, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getVideoLoadedFraction(completion: completion)
+    }
+    
+    /// Returns the state of the player video
+    /// - Parameter completion: The completion closure
+    public func getVideoState(
+        completion: @escaping (Result<VideoState, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getVideoState(completion: completion)
+    }
+    
+    /// Returns the elapsed time in seconds since the video started playing
+    /// - Parameter completion: The completion closure
+    public func getCurrentTime(
+        completion: @escaping (Result<UInt, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getCurrentTime(completion: completion)
+    }
+    
+}
+
+// MARK: - YouTubePlayerVideoInformationAPI
+
+extension YouTubePlayer: YouTubePlayerVideoInformationAPI {
+    
+    /// Returns the duration in seconds of the currently playing video
+    /// - Parameter completion: The completion closure
+    public func getDuration(
+        completion: @escaping (Result<UInt, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getDuration(completion: completion)
+    }
+    
+    /// Returns the YouTube.com URL for the currently loaded/playing video
+    /// - Parameter completion: The completion closure
+    public func getVideoURL(
+        completion: @escaping (Result<String, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getVideoURL(completion: completion)
+    }
+    
+    /// Returns the embed code for the currently loaded/playing video
+    /// - Parameter completion: The completion closure
+    public func getVideoEmbedCode(
+        completion: @escaping (Result<String, YouTubePlayerAPIError>) -> Void
+    ) {
+        self.api?.getVideoEmbedCode(completion: completion)
+    }
+    
+}
