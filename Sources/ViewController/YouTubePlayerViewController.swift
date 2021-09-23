b831fb039cdd197b9bd9bd472d6414edf4285a06
Sven Tiigi <sven.tiigi@gmail.com>
2021-09-23 21:07:44 +0200

    Added macOS support for YouTubePlayerViewController

--- Sources/ViewController/YouTubePlayerViewController.swift
+++ Sources/ViewController/YouTubePlayerViewController.swift
@@ -37,8 +37,53 @@ public final class YouTubePlayerViewController: UIViewController {
     
     // MARK: View-Lifecycle
     
-    /// Creates the view that the controller manages.
+    /// Creates the view that the controller manages
     public override func loadView() {
+        self.view = self.webView
+    }
+    
+}
+#else
+import AppKit
+
+// MARK: - YouTubePlayerViewController
+
+/// The YouTubePlayer UIViewController
+final class YouTubePlayerViewController: NSViewController {
+    
+    // MARK: Properties
+    
+    /// The YouTubePlayerWebView
+    private let webView: YouTubePlayerWebView
+    
+    /// The YouTubePlayer
+    public var player: YouTubePlayer {
+        self.webView.player
+    }
+    
+    // MARK: Initializer
+    
+    /// Creates a new instance of `YouTubePlayer.ViewController`
+    /// - Parameters:
+    ///   - player: The YouTubePlayer
+    public init(
+        player: YouTubePlayer
+    ) {
+        // Initialize WebView
+        self.webView = .init(player: player)
+        // Super init
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    /// Initializer with NSCoder always returns nil
+    required init?(coder: NSCoder) {
+        nil
+    }
+    
+    // MARK: View-Lifecycle
+    
+    /// Creates the view that the controller manages
+    override func loadView() {
         self.view = self.webView
     }
     
