73fa554bc39d3795a4e8ffe2ad01cf3877317232
Sven Tiigi <sven.tiigi@gmail.com>
2023-10-07 09:36:06 +0200

    Unwrap weak self

--- Sources/API/YouTubePlayer+ConfigurationAPI.swift
+++ Sources/API/YouTubePlayer+ConfigurationAPI.swift
@@ -16,30 +16,35 @@ public extension YouTubePlayer {
             javaScript: .player(function: "destroy"),
             converter: .empty
         ) { [weak self] _ in
+            // Verify self is available
+            guard let self = self else {
+                // Otherwise return out of function
+                return
+            }
             // Update YouTubePlayer Configuration
-            self?.configuration = configuration
+            self.configuration = configuration
             // Check if PlayerState Subject has a current value
-            if self?.playerStateSubject.value != nil {
+            if self.playerStateSubject.value != nil {
                 // Reset PlayerState Subject current value
-                self?.playerStateSubject.send(nil)
+                self.playerStateSubject.send(nil)
             }
             // Check if PlaybackState Subject has a current value
-            if self?.playbackStateSubject.value != nil {
+            if self.playbackStateSubject.value != nil {
                 // Reset PlaybackState Subject current value
-                self?.playbackStateSubject.send(nil)
+                self.playbackStateSubject.send(nil)
             }
             // Check if PlaybackQuality Subject has a current value
-            if self?.playbackQualitySubject.value != nil {
+            if self.playbackQualitySubject.value != nil {
                 // Reset PlaybackQuality Subject current value
-                self?.playbackQualitySubject.send(nil)
+                self.playbackQualitySubject.send(nil)
             }
             // Check if PlaybackRate Subject has a current value
-            if self?.playbackRateSubject.value != nil {
+            if self.playbackRateSubject.value != nil {
                 // Reset PlaybackRate Subject current value
-                self?.playbackRateSubject.send(nil)
+                self.playbackRateSubject.send(nil)
             }
             // Re-Load Player
-            self?.webView.load(using: self!)
+            self.webView.load(using: self)
         }
     }
     
